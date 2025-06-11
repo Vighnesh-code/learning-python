2aeb11eb8199b197d4a69ec63588d1b2c5b44aca
Vighnesh-code <vighneshzagade4@gmail.com>
2025-06-11 10:08:58 +0000

    ticket price calculator

--- ticket_price.py
+++ ticket_price.py
@@ -1,15 +1,23 @@
 print("Welcome to the ticket price calculator!")
 
 height = int(input("What's your height? "))
+bill = 0
 
 if (height >= 120):
     print("Yes, you can buy the ticket.")
     age = int(input("What's your age? "))
     if (age <= 12):
-        print("You will have to pay $5")
+        bill = 5
     elif (age <= 18):
-        print("You will have to pay $7")
+        bill = 7
     else:
-        print("You will have to pay $12")
+        bill = 12
+
+    want_photos = input("Do you want photos? Type y for yes and n for no: ")
+    if (want_photos == "y"):
+        bill += 3
+
+    print(f"Here's you final bill ${bill}")
+
 else:
     print("You can't buy the ticket. You are a midget! 😂")
