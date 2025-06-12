88718e1d43d7700eabce143d87ccad1e7bfb3ad0
Vighnesh-code <vighneshzagade4@gmail.com>
2025-06-12 06:05:08 +0000

    Who will pay the bill problem

---
+++ lists.py
@@ -0,0 +1,4 @@
+states_of_america = ["Delaware", "Pennsylvania",
+                     "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hamsphire"]
+
+print(states_of_america[1])
---
+++ who_will_pay_the_bill.py
@@ -0,0 +1,5 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# print(friends[random.randint(0, len(friends)-1)]) one way of doing it.
+print(random.choice(friends))  # another way of doing the same thing.
