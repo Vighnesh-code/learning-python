9f8b9f4875eae9bc4170cc545f306bfe6ae602d8
Vighnesh-code <vighneshzagade4@gmail.com>
2025-06-13 11:06:34 +0000

    pypassword generator

--- pypassword_generator.py
+++ pypassword_generator.py
@@ -13,7 +13,6 @@ nr_letters = int(input("How many letters would you like in your password? \n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
 # Easy Level
 # password = ""
 #
